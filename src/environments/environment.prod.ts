17aea0ce8902eb7fa6fafc598fc8c57bdd5bd99f
MetehanOzalp <metehanozalp@hotmail.com>
2022-03-22 16:26:58 +0300

    refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   apiUrl: 'http://freelancermarket-backend.herokuapp.com/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDuhmaenMb71D9HZ-xW7Gzi5qgsgGz2wr0',
+    authDomain: 'pro3-92e17.firebaseapp.com',
+    projectId: 'pro3-92e17',
+    storageBucket: 'pro3-92e17.appspot.com',
+    messagingSenderId: '231459185601',
+    appId: '1:231459185601:web:51eaf344282efd68b84540',
+  },
 };
